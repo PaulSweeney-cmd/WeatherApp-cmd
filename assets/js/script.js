71a9ef095f22803e7ae2c1fe87632659b98977d1
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-07-25 17:13:24 +0000

    Aded if/else  statement to ring true if users current locatgion exists in the browser

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,18 @@
 window.addEventListener('load', ()=> {
-    let lon;
+    /* defining the co-ordinates variables */
+    let long;
     let lat;
 
-    /* if users current location exists in the browser */
+    /* if users current location exists in the browser get current location */
     if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(position => {
-                console.log(position);
-            });
+        navigator.geolocation.getCurrentPosition(location => {
+            long = location.coords.longitude;
+            lat = location.coords.latitude;
+
+            /* assigning the API call with your generated API key to the variable, 
+            API key sourced from www.OpenWeather.org */
+            const apiKey = `api.openweathermap.org/data/2.5/weather?lat=${lat},&lon=${long}&appid={1b1739c497edc04ee44df7be05100696}`;
+        });
     
     /* if the users curent location does not exists, produce the following message */
     }else {
@@ -17,4 +23,3 @@ window.addEventListener('load', ()=> {
 
 
 
-// const api = `api.openweathermap.org/data/2.5/weather?lat=${latitude},&lon=${longitude}&appid={1b1739c497edc04ee44df7be05100696}`;
