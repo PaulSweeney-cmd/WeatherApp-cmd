bb07f778d643b99e911175aa9e04c1a609a51f7b
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-07-26 17:58:51 +0000

    Removed all HTML content due to new design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,3 @@
-window.addEventListener('load', ()=> {
-    /* defining the co-ordinates variables */
-    let long;
-    let lat;
-
-    /* if users current location exists in the browser get current location */
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(location => {
-            long = location.coords.longitude;
-            lat = location.coords.latitude;
-
-            /* assigning the API call with your generated API key to the variable, 
-            API key sourced from www.OpenWeather.org */
-            const apiKey = `https://api.openweathermap.org/data/2.5/weather?${lat}&lon=${long}&appid={}`;
-
-            /* getting the location from the URL above by making a call to the api and returning the results */
-            fetch(apiKey)
-                .then(feedback => {
-                    return feedback.json();
-                })
-                .then(data => {
-                    console.log(data);
-            });
-        });
-    }
-});
-
 
 
 
