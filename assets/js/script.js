9325a95d32576924f05eda9d5ee3d880969e0524
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-07-27 18:24:25 +0000

    Assigned api to a variable, created a function with fetch method to get current weather infor for location

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,15 @@
+/* defining a variable complete with your API key assigned as a key:value pair,
+   key sourced from www.openweather.org */
+let weather = {
+    "api": "dbc479491b0ccb704e65731224cbf39d",
 
+    /* creating a function with a fetch method to extract information from the url */
+    getWeather: function () {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=Lincoln&units=metric&appid=dbc479491b0ccb704e65731224cbf39d"
+        ).then((response) => response.json()).then((data) => console.log(data));
+    },
+};
 
 
 
