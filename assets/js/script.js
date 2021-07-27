1b295d6d514a93fe69df2356c1e8b84e23080015
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-07-27 21:22:43 +0000

    Created a variable in js file for the config.js file with the API key included, updated the weather variable with with myapi variable tied to the config file containing key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,37 @@
-/* defining a variable complete with your API key assigned as a key:value pair,
+var myapi = config.MY_API;
+
+/* defining a variable complete with your API key assigned,
    key sourced from www.openweather.org */
 let weather = {
-    "api": "dbc479491b0ccb704e65731224cbf39d",
-
-    /* creating a function with a fetch method to extract information from the url */
-    getWeather: function () {
+    "api": myapi,
+    
+    /* function with a fetch method to extract information from the url */
+    getWeather: function(city){
         fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=Lincoln&units=metric&appid=dbc479491b0ccb704e65731224cbf39d"
-        ).then((response) => response.json()).then((data) => console.log(data));
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + city 
+            + "&units=metric&appid=" 
+            + this.api
+        )
+        .then((response) => response.json())
+        .then((data) => this.showWeather(data));
+    },
+    /* function to extract the json data from the api call & display the weather */
+    showWeather: function(data){
+        /* extract the data from the json object and assign to variables */
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed)
+
+        /* calling the html classes and rendering the data to the app */
+        document.querySelector(".city").innerText = " " + name;
+        document.querySelector(".weather-icon").src = "https://openweathermap.org/img/wn/"+ icon +"@2x.png";
+        document.querySelector(".conditions").innerText = description;
+        document.querySelector(".temperature").innerText = temp + " °C";
+        document.querySelector(".humidity").innerText = "Humidity :" + humidity + " %";
+        document.querySelector(".wind-speed").innerText = "Wind Speed :" + speed + " km/h";
     },
 };
 
